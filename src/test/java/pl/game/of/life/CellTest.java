d250f5b967623404a15115b017a1961e3fd758b0
Piotr Bartoch <piotr.bartoch@gmail.com>
2017-04-14 19:08:17 +0200

    Tests added, last condition to fulfill left

--- src/test/java/pl/game/of/life/CellTest.java
+++ src/test/java/pl/game/of/life/CellTest.java
@@ -26,7 +26,6 @@ public class CellTest {
         assertThat(cell.getState()).isFalse();
     }
 
-
     @Test
     public void should_die_when_has_four_or_more_neighbors() {
         cell.setNewState(4);
@@ -37,7 +36,34 @@ public class CellTest {
 
         cell.setNewState(150);
         assertThat(cell.getState()).isFalse();
-
-
     }
+
+    @Test
+    public void should_survive_when_has_two_neighbors() {
+        cell.setNewState(2);
+        assertThat(cell.getState()).isTrue();
+    }
+
+    @Test
+    public void should_survive_when_has_three_neighbors() {
+        cell.setNewState(3);
+        assertThat(cell.getState()).isTrue();
+    }
+
+    @Test
+    public void should_be_populated_when_it_is_dead_and_have_three_neighbors() {
+        Cell deadCell = new Cell(false);
+        assertThat(deadCell.getState()).isFalse();
+        deadCell.setNewState(3);
+        assertThat(deadCell.getState()).isTrue();
+    }
+
+    @Test
+    public void should_stay_dead_when_it_is_dead_and_have_two_neighbors() {
+        Cell deadCell = new Cell(false);
+        assertThat(deadCell.getState()).isFalse();
+        deadCell.setNewState(2);
+        assertThat(deadCell.getState()).isFalse();
+    }
+
 }
