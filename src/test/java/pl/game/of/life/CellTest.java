5cbe8a71d8ea26ba49286284712f867701cb3fe8
Piotr Bartoch <piotr.bartoch@gmail.com>
2017-04-17 16:15:56 +0200

    Tests refactored, splitted for dead and alive cells

--- src/test/java/pl/game/of/life/CellTest.java
+++ src/test/java/pl/game/of/life/CellTest.java
@@ -10,45 +10,50 @@ import static org.assertj.core.api.Assertions.*;
  */
 public class CellTest {
 
-    private Cell cell;
+    private Cell aliveCell;
+
+    /**    ---Alive Cells------------------------------------------------------------------------------------- */
 
     @Before
     public void setUp() {
-        cell = new Cell(true);
+        aliveCell = new Cell(true);
     }
 
     @Test
-    public void should_die_when_has_one_or_zero_neighbors() {
-        cell.setNewState(0);
-        assertThat(cell.getState()).isFalse();
+    public void should_die_when_is_alive_and_has_one_or_zero_neighbors() {
+        aliveCell.setNewState(0);
+        assertThat(aliveCell.getState()).isFalse();
 
-        cell.setNewState(1);
-        assertThat(cell.getState()).isFalse();
+        aliveCell.setNewState(1);
+        assertThat(aliveCell.getState()).isFalse();
     }
 
     @Test
-    public void should_die_when_has_four_or_more_neighbors() {
-        cell.setNewState(4);
-        assertThat(cell.getState()).isFalse();
+    public void should_die_when_is_alive_and_has_four_or_more_neighbors() {
+        aliveCell.setNewState(4);
+        assertThat(aliveCell.getState()).isFalse();
 
-        cell.setNewState(5);
-        assertThat(cell.getState()).isFalse();
+        aliveCell.setNewState(5);
+        assertThat(aliveCell.getState()).isFalse();
 
-        cell.setNewState(150);
-        assertThat(cell.getState()).isFalse();
+        aliveCell.setNewState(150);
+        assertThat(aliveCell.getState()).isFalse();
     }
 
     @Test
-    public void should_survive_when_has_two_neighbors() {
-        cell.setNewState(2);
-        assertThat(cell.getState()).isTrue();
+    public void should_survive_when_is_alive_and_has_two_or_three_neighbors() {
+        aliveCell.setNewState(2);
+        assertThat(aliveCell.getState()).isTrue();
+
+        aliveCell.setNewState(3);
+        assertThat(aliveCell.getState()).isTrue();
     }
 
-    @Test
-    public void should_survive_when_has_three_neighbors() {
-        cell.setNewState(3);
-        assertThat(cell.getState()).isTrue();
-    }
+
+    /**    ---Dead Cells------------------------------------------------------------------------------------- */
+
+    @Before
+
 
     @Test
     public void should_be_populated_when_it_is_dead_and_have_three_neighbors() {
@@ -59,7 +64,7 @@ public class CellTest {
     }
 
     @Test
-    public void should_stay_dead_when_it_is_dead_and_have_two_neighbors() {
+    public void should_stay_dead_when_it_is_dead_and_have_two_neighbors() { // todo: make it pass, and test all cases
         Cell deadCell = new Cell(false);
         assertThat(deadCell.getState()).isFalse();
         deadCell.setNewState(2);
